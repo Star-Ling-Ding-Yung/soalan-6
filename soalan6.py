a302ab7f070b31afbc1d59fbbd0b5fda13a0c58a
Star-Ling-Ding-Yung <163367533+Star-Ling-Ding-Yung@users.noreply.github.com>
2024-04-04 12:13:40 +0800

    Create soalan6.py

---
+++ soalan6.py
@@ -0,0 +1,13 @@
+markah=int(input("Sila masukkan markah murid:"))
+
+if 0<= markah <=40:
+    print("Anda ditempatkan di kelas Dedikasi")
+    
+elif 41<= markah <=60:
+    print("Anda ditempatkan di kelas Cerdik")
+
+elif 61<= markah <=80:
+    print("Anda ditempatkan di kelas Bijak")
+
+else:
+    print("Anda ditempatkan di kelas Amanah")
